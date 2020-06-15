71ae0c2e44210d09d56c518d539e2f5f4ea0530b
richard-scryber <richard@scryber.co.uk>
2020-06-15 09:14:10 +0100

    Added Parser Test Classes

---
+++ Scryber.Core.UnitTest/Generation/ParserTestClasses.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scryber.Core.UnitTests.Generation.Fakes
+{
+    //
+    // These classes are used by the Parser tests to validate the attribute
+    // reflection and xml parser.
+    //
+
+
+    /// <summary>
+    /// A top level root component that can be reflected and inspected
+    /// </summary>
+    /// 
+    [PDFParsableComponent("Root1")]
+    [PDFRequiredFramework("0.8.0.0","1.0.0.0")]
+    public class ParserRootOne : IPDFComponent
+    {
+        /// <summary>
+        /// Simple read write name attribute
+        /// </summary>
+        [PDFAttribute("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// An inner complex component
+        /// </summary>
+        [PDFElement("Complex-Element")]
+        public ParserInnerComplex Complex { get; set; }
+
+        /// <summary>
+        /// A collection of items within a surrounding element
+        /// </summary>
+        [PDFArray(typeof(ParserInnerComplex))]
+        [PDFElement("Collection-One")]
+        public ParserCollection CollectionOne { get; set; }
+
+        /// <summary>
+        /// A collection of items that are the default
+        /// </summary>
+        [PDFArray(typeof(ParserInnerBase))]
+        [PDFElement("")]
+        public ParserCollection DefaultCollection { get; set; }
+
+
+        #region IPDFComponent implementation for parsing - don't use
+
+        public event PDFInitializedEventHandler Initialized;
+
+        void IPDFComponent.Init(PDFInitContext context)
+        {
+            if (null != this.Initialized)
+                this.Initialized(this, new PDFInitEventArgs(context));
+        }
+
+
+        public event PDFLoadedEventHandler Loaded;
+
+        public void Load(PDFLoadContext context)
+        {
+            if (null != this.Loaded)
+                this.Loaded(this, new PDFLoadEventArgs(context));
+        }
+
+
+        string IPDFComponent.ID
+        {
+            get
+            {
+                return string.Empty;
+            }
+            set
+            {
+                
+            }
+        }
+
+        IPDFDocument IPDFComponent.Document
+        {
+            get { return null; }
+        }
+
+        IPDFComponent IPDFComponent.Parent
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+                
+            }
+        }
+
+        string IPDFComponent.MapPath(string source)
+        {
+            return source;
+        }
+
+        PDFObjectType ITypedObject.Type
+        {
+            get { return (PDFObjectType)"naob"; }
+        }
+
+        void IDisposable.Dispose()
+        {
+            
+        }
+
+        #endregion
+    }
+
+    public abstract class ParserInnerBase
+    {
+        /// <summary>
+        /// Integer value
+        /// </summary>
+        [PDFAttribute("index")]
+        public int Index { get; set; }
+
+        /// <summary>
+        /// No Attribute - no inclusion
+        /// </summary>
+        public int AnotherIndex { get; set; }
+
+        /// <summary>
+        /// string value that can be overriden
+        /// </summary>
+        [PDFAttribute("inherited")]
+        public virtual string Inheritable { get; set; }
+
+    }
+
+    /// <summary>
+    /// Concrete implementation of the abstract base class
+    /// </summary>
+    [PDFParsableComponent("Inner")]
+    [PDFRemoteParsableComponent("Inner-Ref")]
+    public class ParserInnerComplex : ParserInnerBase, IPDFComponent
+    {
+        /// <summary>
+        /// Overrides the base implementation
+        /// </summary>
+        [PDFAttribute("concrete", ParserDefinitionFactoryTests.NamespaceAssembly)]
+        public override string Inheritable
+        {
+            get
+            {
+                return base.Inheritable;
+            }
+            set
+            {
+                base.Inheritable = value;
+            }
+        }
+
+        /// <summary>
+        /// Double value
+        /// </summary>
+        [PDFAttribute("size")]
+        public double Size { get; set; }
+
+        [PDFAttribute("date")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Double value that cannot be written to.
+        /// </summary>
+        [PDFAttribute("ro-size")]
+        public double ReadOnlySize
+        {
+            get { return Size; }
+        }
+
+
+        #region IPDFComponent implementation for parsing - Needed for remote reference
+
+        public event PDFInitializedEventHandler Initialized;
+
+        void IPDFComponent.Init(PDFInitContext context)
+        {
+            if (null != this.Initialized)
+                this.Initialized(this, new PDFInitEventArgs(context));
+        }
+
+        
+        public event PDFLoadedEventHandler Loaded;
+
+        public void Load(PDFLoadContext context)
+        {
+            if(null != this.Loaded)
+                this.Loaded(this, new PDFLoadEventArgs(context));
+        }
+
+        string IPDFComponent.ID
+        {
+            get
+            {
+                return string.Empty;
+            }
+            set
+            {
+
+            }
+        }
+
+        IPDFDocument IPDFComponent.Document
+        {
+            get { return null; }
+        }
+
+        IPDFComponent IPDFComponent.Parent
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+
+            }
+        }
+
+        string IPDFComponent.MapPath(string source)
+        {
+            return source;
+        }
+
+        PDFObjectType ITypedObject.Type
+        {
+            get { return (PDFObjectType)"naob"; }
+        }
+
+        void IDisposable.Dispose()
+        {
+
+        }
+
+        #endregion
+    }
+
+
+    public class ParserCollection : List<ParserInnerBase>
+    {
+    }
+
+
+    [PDFParsableComponent("Invalid-Inherits")]
+    public class InvalidAttributeInherits
+    {
+        /// <summary>
+        /// Will throw execption as inherits is a reserved attribute name
+        /// </summary>
+        [PDFAttribute("inherits")]
+        public int Inherits { get; set; }
+    }
+
+    [PDFParsableComponent("Invalid-Code")]
+    public class InvalidAttributeCode
+    {
+        /// <summary>
+        /// Will throw execption as code-file is a reserved attribute name
+        /// </summary>
+        [PDFAttribute("code-file")]
+        public int CodeFile { get; set; }
+    }
+
+}
+
+
+// another class in different namesapce
+
+namespace Scryber.Core.UnitTests.Generation.Fakes.More
+{
+    [PDFParsableComponent("DifferentNS")]
+    public class ParserDifferentComplex : ParserInnerBase
+    {
+        /// <summary>
+        /// A simple value element
+        /// </summary>
+        [PDFElement("Simple")]
+        public Scryber.Drawing.PDFThickness Thickness { get; set; }
+    }
+}
