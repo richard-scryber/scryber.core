6ee430374020c88073a4603d2b178f2a4af2abf5
richard-scryber <richard@scryber.co.uk>
2020-06-19 18:23:21 +0100

    Added the Controller Extension methods with model parameters, and updated the version numbers for the Nuget packages for release.

--- Scryber.Components.Mvc/ControllerExtensions.cs
+++ Scryber.Components.Mvc/ControllerExtensions.cs
@@ -12,6 +12,8 @@ namespace Scryber.Components.Mvc
 
         private const string PDFMimeType = "application/pdf";
 
+        #region No Model
+
         /// <summary>
         /// Generate and return a PDF document file result from the template at the specified document path
         /// </summary>
@@ -20,7 +22,7 @@ namespace Scryber.Components.Mvc
         /// <param name="inline">If true, then the output PDF will be directly streamed to the output, otherwise it will be as an attachment</param>
         /// <param name="outputFileName">The name of the file to be downloaded if it is an attachment, if not set the document file name or controller request action name will be used</param>
         /// <returns>A file action result with the appropriate headers and content type</returns>
-        public static IActionResult PDF(this ControllerBase controller, string fullpath, bool inline = true, string outputFileName = "")
+        public static IActionResult PDF(this ControllerBase controller,  string fullpath, bool inline = true, string outputFileName = "")
         {
             var doc = PDFDocument.ParseDocument(fullpath);
             if(inline == false)
@@ -71,6 +73,82 @@ namespace Scryber.Components.Mvc
             else
                 return controller.File(ms, PDFMimeType);
         }
+
+        #endregion
+
+
+        #region Generic type model
+
+        /// <summary>
+        /// Generate and return a PDF document file result from the template at the specified document path
+        /// </summary>
+        /// <typeparam name="T">The generic type declaration for the model to be passed to the document when binding</typeparam>
+        /// <param name="controller">Self reference to the controller as an extension method</param>
+        /// <param name="fullpath">The full path to the document in the file system</param>
+        /// <param name="model">The instance of the model to use when binding</param>
+        /// <param name="inline">If true, then the output PDF will be directly streamed to the output, otherwise it will be as an attachment</param>
+        /// <param name="outputFileName">The name of the file to be downloaded if it is an attachment, if not set the document file name or controller request action name will be used</param>
+        /// <returns>A file action result with the appropriate headers and content type</returns>
+        public static IActionResult PDF<T>(this ControllerBase controller, string fullpath, T model, bool inline = true, string outputFileName = "")
+        {
+            var doc = PDFDocument.ParseDocument(fullpath);
+            if (inline == false)
+            {
+                if (string.IsNullOrEmpty(outputFileName))
+                    outputFileName = System.IO.Path.GetFileNameWithoutExtension(fullpath) + ".pdf";
+            }
+
+            return PDF(controller, doc, model, inline, outputFileName);
+        }
+
+        /// <summary>
+        /// Generate and return a PDF document file result from the template in the content stream
+        /// </summary>
+        /// <typeparam name="T">The generic type declaration for the model to be passed to the document when binding</typeparam>
+        /// <param name="controller">Self reference to the controller as an extension method</param>
+        /// <param name="content">The xml template content within the stream</param>
+        /// <param name="model">The instance of the model to use when binding</param>
+        /// <param name="inline">If true, then the output PDF will be directly streamed to the output, otherwise it will be as an attachment</param>
+        /// <param name="outputFileName">The name of the file to be downloaded if it is an attachment, if not set the document file name or controller request action name will be used</param>
+        /// <returns>A file action result with the appropriate headers and content type</returns>
+        public static IActionResult PDF<T>(this ControllerBase controller, System.IO.Stream content, T model, bool inline = true, string outputFileName = "")
+        {
+            var doc = PDFDocument.ParseDocument(content, ParseSourceType.DynamicContent);
+
+            return PDF(controller, doc, model, inline, outputFileName);
+        }
+
+        /// <summary>
+        /// Generate and return a PDF document file result from the PDFDocument instance
+        /// </summary>
+        /// <typeparam name="T">The generic type declaration for the model to be passed to the document when binding</typeparam>
+        /// <param name="controller">Self reference to the controller as an extension method</param>
+        /// <param name="document">The PDF document to render to the output stream</param>
+        /// <param name="model">The instance of the model to use when binding</param>
+        /// <param name="inline">If true, then the output PDF will be directly streamed to the output, otherwise it will be as an attachment</param>
+        /// <param name="outputFileName">The name of the file to be downloaded if it is an attachment, if not set the document file name or controller request action name will be used</param>
+        /// <returns>A file action result with the appropriate headers and content type</returns>
+        public static IActionResult PDF<T>(this ControllerBase controller, PDFDocument document, T model, bool inline = true, string outputFileName = "")
+        {
+            var ms = new System.IO.MemoryStream();
+            document.Items["Model"] = model;
+            document.Items["Controller"] = controller;
+
+            document.ProcessDocument(ms);
+            ms.Flush();
+
+            ms.Position = 0;
+            if (!inline)
+            {
+                if (string.IsNullOrEmpty(outputFileName))
+                    outputFileName = GetDocumentName(document, controller);
+                return controller.File(ms, PDFMimeType, outputFileName);
+            }
+            else
+                return controller.File(ms, PDFMimeType);
+        }
+
+        #endregion
 
         private static string GetDocumentName(PDFDocument doc, ControllerBase controller)
         {
