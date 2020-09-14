7518b32f824d9b40b7e4c7ae16b631b571810ad8
Richard Hewitson <Richard.Hewitson@SiliconReef.co.uk>
2020-09-14 15:50:34 +0100

    Local commit of the branch before doing anything

---
+++ Scryber.Components/Svg/Components/SVGEllipse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scryber.Components;
+
+namespace Scryber.Svg.Components
+{
+    public class SVGEllipse : Ellipse
+    {
+
+    }
+}
