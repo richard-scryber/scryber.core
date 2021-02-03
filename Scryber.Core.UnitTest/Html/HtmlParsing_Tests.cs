99e6647233ab157f318b1c0315fe16325e73a877
richard-scryber <richard@scryber.co.uk>
2021-02-03 11:59:56 +0000

    Updated documentation and dll file version to match packages.

--- Scryber.Core.UnitTest/Html/HtmlParsing_Tests.cs
+++ Scryber.Core.UnitTest/Html/HtmlParsing_Tests.cs
@@ -738,6 +738,18 @@ namespace Scryber.Core.UnitTests.Html
                 }
 
             }
+
+            using (var doc = Document.ParseDocument(path))
+            {
+                //pass paramters as needed, supporting simple values, arrays or complex classes.
+
+                using (var stream = DocStreams.GetOutputStream("documentation.pdf"))
+                {
+
+                    doc.SaveAsPDF(stream);
+                }
+
+            }
         }
 
         [TestMethod()]
