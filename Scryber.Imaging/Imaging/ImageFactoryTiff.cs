8a629592b675acb1c9bb2c181654f539413c3b0d
richard-scryber <richard@scryber.co.uk>
2021-10-06 18:42:23 +0100

    Removed ImageSharp

---
+++ Scryber.Imaging/Imaging/ImageFactoryTiff.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Scryber.Drawing;
+using Scryber.Imaging.Formatted;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+
+namespace Scryber.Imaging
+{
+    public class ImageFactoryPng : ImageFactoryBase, IPDFImageDataFactory
+    {
+        public ImageFactoryPng()
+        {
+        }
+
+        public override bool ShouldCache
+        {
+            get { return true; }
+        }
+
+
+        protected override PDFImageData DoDecodeImageData(Stream stream, IDocument document, IComponent owner, string path)
+        {
+            IImageFormat format;
+            SixLabors.ImageSharp.Configuration config = SixLabors.ImageSharp.Configuration.Default;
+            var img = Image.Load(config, stream, out format);
+
+            PDFImageData data = null;
+
+            if (format.Name == "PNG")
+            {
+                data = GetImageDataForImage(img, path);
+            }
+
+
+
+            return data;
+        }
+
+
+
+
+    }
+}
