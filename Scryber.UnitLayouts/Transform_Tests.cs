0633a2b3e782bd9cbbf74c6e194ed7d1eeec2352
Richard Hewitson <richard@scryber.co.uk>
2024-09-23 19:05:43 +0100

    Added placeholder file for the layout tests for transforms

---
+++ Scryber.UnitLayouts/Transform_Tests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Tar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scryber.Components;
+using Scryber.PDF.Layout;
+using Scryber.PDF;
+using Scryber.Drawing;
+using Scryber.PDF.Resources;
+using Scryber.Svg.Components;
+
+namespace Scryber.UnitLayouts
+{
+    [TestClass()]
+    public class Transform_Tests
+    {
+        const string TestCategoryName = "Layout Transform";
+
+        PDFLayoutDocument layout;
+
+        /// <summary>
+        /// Event handler that sets the layout instance variable, after the layout has completed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void Doc_LayoutComplete(object sender, LayoutEventArgs args)
+        {
+            layout = args.Context.GetLayout<PDFLayoutDocument>();
+        }
+
+
+        protected string AssertGetContentFile(string name)
+        {
+            var path = System.Environment.CurrentDirectory;
+            path = System.IO.Path.Combine(path, "../../../Content/HTML/Transform/" + name + ".html");
+            path = System.IO.Path.GetFullPath(path);
+
+            if (!System.IO.File.Exists(path))
+                Assert.Inconclusive("The path the file " + name + " was not found at " + path);
+
+            return path;
+        }
+
+        [TestCategory(TestCategoryName)]
+        [TestMethod()]
+        public void Transform_01_Empty()
+        {
+            var path = AssertGetContentFile("SVG_01_Empty");
+
+            var doc = Document.ParseDocument(path);
+
+            using (var ms = DocStreams.GetOutputStream("SVG_01_Empty.pdf"))
+            {
+                doc.Pages[0].Style.OverlayGrid.ShowGrid = true;
+                doc.Pages[0].Style.OverlayGrid.GridSpacing = 10;
+                doc.Pages[0].Style.OverlayGrid.GridColor = StandardColors.Aqua;
+                doc.Pages[0].Style.OverlayGrid.GridMajorCount = 5;
+                doc.RenderOptions.Compression = OutputCompressionType.None;
+                
+
+                doc.LayoutComplete += Doc_LayoutComplete;
+                doc.SaveAsPDF(ms);
+            }
+
+            Assert.Inconclusive("Transform tests still to do");
+            
+            
+            
+        }
+        
+    }
+}
